3685dcec9d54ae3fc4961c85b43f88dc9348a955
Mina Soultan <minasoltan@windowslive.com>
2022-03-21 15:49:56 +0200

    adding initial components

---
+++ src/containers/BookShelf.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class BookShelf extends Component {
+  render() {
+    return (
+      <div>This is BookShelf</div>
+    )
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
 
-ReactDOM.render(<App />, document.getElementById('root'))
+  document.getElementById("root")
+);
